7c12f1bec54caa8a0a96480515b1ae51b9145749
Gokul <gokul.k@spurtreetech.in>
2025-04-29 16:09:46 +0530

    updated the port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Backend is running!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
